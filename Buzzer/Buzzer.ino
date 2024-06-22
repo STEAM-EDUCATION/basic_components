a12fd68d3b7e0f1cc831da294644b793e9db80af
Sahan Dankelewaththa <120778675+RPSTD@users.noreply.github.com>
2024-06-22 12:15:51 +0530

    Create Buzzer.ino

---
+++ Buzzer/Buzzer.ino
@@ -0,0 +1,12 @@
+const int buzzer = 9; 
+
+void setup(){
+  pinMode(buzzer, OUTPUT); 
+}
+
+void loop(){
+  tone(buzzer, 1000); // Send 1KHz sound signal...
+  delay(1000);
+  noTone(buzzer);     // Stop sound...
+  delay(1000);
+}
